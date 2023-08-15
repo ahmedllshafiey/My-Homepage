1e7e08b07229e2899b6cd7e53cfa470eaf743da0
ahmedlldev <ahmedll.shafiey@gmail.com>
2023-08-15 07:36:16 +0300

    NextLink and Chakra-Ui Link issue Fixed!!

--- components/navbar.js
+++ components/navbar.js
@@ -20,15 +20,15 @@ const LinkItem = ({ href, path, children }) => {
   const active = path === href
   const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
   return (
-    <NextLink href={href}>
-      <Link
-        p={2}
-        bg={active ? 'glassTeal' : undefined}
-        color={active ? '#202023' : inactiveColor}
-      >
-        {children}
-      </Link>
-    </NextLink>
+    <Link
+      as={NextLink}
+      href={href}
+      p={2}
+      bg={active ? 'glassTeal' : undefined}
+      color={active ? '#202023' : inactiveColor}
+    >
+      {children}
+    </Link>
   )
 }
 
@@ -63,7 +63,7 @@ const Navbar = props => {
           width={{ base: 'full', md: 'auto' }}
           alignItems="center"
           flexGrow={1}
-          mt={{ base: 4, nmd: 0 }}
+          mt={{ base: 4, md: 0 }}
         >
           <LinkItem href="/works" path={path}>
             Works
@@ -82,18 +82,18 @@ const Navbar = props => {
                 aria-label="Options"
               />
               <MenuList>
-                <NextLink href="/" passHref>
-                  <MenuItem as={Link}>About</MenuItem>
-                </NextLink>
-                <NextLink href="/works" passHref>
-                  <MenuItem as={Link}>Works</MenuItem>
-                </NextLink>
-                <NextLink href="/posts" passHref>
-                  <MenuItem as={Link}>Posts</MenuItem>
-                </NextLink>
-                <NextLink href="/" passHref>
-                  <MenuItem as={Link}>View Source Code</MenuItem>
-                </NextLink>
+                <MenuItem as={NextLink} href="/">
+                  About
+                </MenuItem>
+                <MenuItem as={NextLink} href="/">
+                  Works
+                </MenuItem>
+                <MenuItem as={NextLink} href="/">
+                  Posts
+                </MenuItem>
+                <MenuItem as={NextLink} href="/">
+                  Code Source
+                </MenuItem>
               </MenuList>
             </Menu>
           </Box>
