73267d6364c5b67d871ac35a0a795484889ddc2e
ahmedlldev <ahmedll.shafiey@gmail.com>
2023-08-21 14:51:00 +0300

    Error Page added and Project Page edited

--- pages/works.js
+++ pages/works.js
@@ -1,23 +1,19 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
-import eidthumbnail from '../public/images/assets/Eid.jpg'
+import health from '../public/images/assets/Convoy.jpg'
 
 const Works = () => {
   return (
     <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Graphic Design
+      <Heading as="h3" fontSize={25} mb={4}>
+        Humanitarian Projects
       </Heading>
       <SimpleGrid columns={[1, 1, 1]} gap={6}>
-        <WorkGridItem id="Eid" title="Eid" thumbnail={eidthumbnail}>
-          An Islamic Eid festival design designed using Adobe Photoshop.
-        </WorkGridItem>
-      </SimpleGrid>
-      <br/>
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <WorkGridItem id="Eid" title="Eid" thumbnail={eidthumbnail}>
-          An Islamic Eid festival design designed using Adobe Photoshop.
+        <WorkGridItem id="heath" title="Health Humans" thumbnail={health}>
+          Volunteering Medical conference hosted and organized by EPSF-Beni
+          Suef. The aim of the conference was improving awareness of
+          cardiovascular diseases.
         </WorkGridItem>
       </SimpleGrid>
     </Container>
